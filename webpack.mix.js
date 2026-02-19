1c9cd7b394b6516960d1472ad3624c3e7d4a189e
naum-code <naummaster87@yandex.ru>
2026-02-19 09:02:28 +0500

    add start content to webpack.mix.js

---
+++ webpack.mix.js
@@ -0,0 +1,28 @@
+const mix = require("laravel-mix");
+
+mix.disableNotifications();
+mix.setPublicPath("app/static");
+mix.sourceMaps(false, "source-map");
+mix.extract();
+mix.version();
+
+const postcssPlugins = [require("postcss-sort-media-queries")()];
+
+if (mix.inProduction()) {
+  postcssPlugins.push(require("postcss-combine-buplicated-selectors")());
+}
+
+mix.options({
+  postCss: postcssPlugins,
+});
+
+mix.browserSync({
+  proxy: "localhost:8000",
+  open: false,
+  notify: false,
+  files: ["app/templates/**/*.html"],
+});
+
+mix.copyDirectory("src/static", "app/static")
+mix.js('src/js/main.js', 'js')
+mix.sass('src/scss/main.scss', 'css')
