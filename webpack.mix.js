877d7b80efee7c5d65da62ced6f3aeaedf049bd2
naum-code <naummaster87@yandex.ru>
2026-02-19 22:27:24 +0500

    end of experiment to laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,30 +1,39 @@
 const mix = require("laravel-mix");
 
-mix.disableNotifications();
-mix.setPublicPath("www/content/skin");
-mix.sourceMaps(false, "source-map");
-mix.extract();
-mix.version();
+mix
+  .disableNotifications()
+  .setPublicPath("www/content")
+  .sourceMaps(false, "source-map")
+  .version();
 
-const postcssPlugins = [require("postcss-sort-media-queries")()];
+const postcssPlugins = [
+  require("postcss-sort-media-queries")(),
+  require("autoprefixer")({
+    overrideBrowserslist: ["last 5 versions"],
+    grid: true,
+  }),
+];
 
 if (mix.inProduction()) {
-  postcssPlugins.push(require("postcss-combine-buplicated-selectors")());
+  postcssPlugins.push(require("postcss-combine-duplicated-selectors")());
 }
 
 mix.options({
   postCss: postcssPlugins,
 });
 
+mix.js("dev_src/scripts", "skin/js");
+mix.sass("dev_src/styles", "skin");
+
+mix.copyDirectory("dev_src/icons", "www/content/skin/icons");
+
 mix.browserSync({
-  //  proxy: "localhost:8000",
   server: {
     baseDir: "./www/content",
     index: "home.htm",
   },
-  files: ["./www/content/*.htm", "./www/content/skin/**/*.css"],
+  files: [
+    "./www/content/*.htm",
+    "./www/content/skin/**/*.css"
+  ],
 });
-
-mix.copyDirectory("dev_src/icons", "www/content/skin/css");
-mix.js("dev_src/scripts/main.js", "www/content/skin");
-mix.sass("dev_src/styles/kakaha.scss", "www/content/skin/css");
